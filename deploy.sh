4e716f7ffed6fb169cd77cfdc2accb7dc2aaa0df
Shaashvat Mittal <shaashvatmittal2004@gmail.com>
2026-01-03 19:16:36 +0530

    ready

--- deploy.sh
+++ deploy.sh
@@ -420,13 +420,13 @@ EOF
         BUILD_IMAGE="aws/codebuild/amazonlinux-x86_64-standard:5.0"
         COMPUTE_TYPE="BUILD_GENERAL1_SMALL"
         PRIVILEGED_MODE="true"
-        SOURCE_VERSION="dev2"
+        SOURCE_VERSION="main"
         BUILDSPEC_FILE="buildspec-unified.yml"
     else
         BUILD_IMAGE="aws/codebuild/amazonlinux2-x86_64-standard:5.0"
         COMPUTE_TYPE="BUILD_GENERAL1_LARGE"
         PRIVILEGED_MODE="true"
-        SOURCE_VERSION="dev2"
+        SOURCE_VERSION="main"
         BUILDSPEC_FILE="buildspec-unified.yml"
     fi
 
